292575f240c912f145aefa088746fc31ac9226d0
Fikriwib12 <wibfikri@gmail.com>
2024-09-18 13:11:50 +0700

    update layout home page

--- front-page.php
+++ front-page.php
@@ -1,22 +1,5 @@
 <?php get_header(); ?>
-<div class="nav-contact" style="margin-top: 20px;">
-    <div class="nav-contact-icon">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/TelephoneOutbound.svg" alt="">
-        <span>021 8690 6777</span>
-    </div>
-    <div class="nav-contact-icon">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Whatsapp.svg" alt="">
-        <span>0852-1556-0669 (Mr. Almas)</span>
-    </div>
-    <div class="nav-contact-icon">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Whatsapp.svg" alt="">
-        <span>0819‑4401‑4959(Mr. Arya)</span>
-    </div>
-    <div class="nav-contact-icon">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mail.svg" alt="">
-        <span>marketing.time@taharica.com</span>
-    </div>
-</div>
+
 
 <div class="nav-search-bar">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-timeinstrument.png" alt="">
@@ -24,9 +7,7 @@
         <input type="search" name="" id="" placeholder="Search Product">
         <button><img src="<?php echo get_template_directory_uri(); ?>/assets/img/search-icon.svg" alt=""></button>
     </div>
-    <div class="avatar">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/avatar_icon.svg" alt="">
-    </div>
+
 </div>
 
 <nav>
@@ -56,16 +37,16 @@
     </div>
 </section>
 
-<section id="container-tab1">
-    <div class="tab">
-        <button class="tablinks tab-active">DISCOUNTS AND PROMOTIONS</button>
-        <button class="tablinks">NEW</button>
-        <button class="tablinks">FEATURED</button>
-        <div class="line"></div>
+<section class="container-tab">
+    <div class="tab" data-tabset="1">
+        <button class="tablinks tab-active" data-tabset="1">DISCOUNTS AND PROMOTIONS</button>
+        <button class="tablinks" data-tabset="1">NEW</button>
+        <button class="tablinks" data-tabset="1">FEATURED</button>
+        <div class="line" data-tabset="1"></div>
     </div>
-    <div class="tabcontent-container">
+    <div class="tabcontent-container" data-tabset="1">
 
-        <div id="discount" class="tabcontent tab-active">
+        <div class="tabcontent tab-active" data-tabset="1">
             <div class="card-container">
                 <div class="card">
                     <div class="card-img">
@@ -114,12 +95,12 @@
             </div>
         </div>
 
-        <div id="new" class="tabcontent">
+        <div class="tabcontent" data-tabset="1">
             <h3>Paris</h3>
             <p>Paris is the capital of France.</p>
         </div>
 
-        <div id="featured" class="tabcontent">
+        <div class="tabcontent" data-tabset="1">
             <h3>Tokyo</h3>
             <p>Tokyo is the capital of Japan.</p>
         </div>
@@ -134,6 +115,168 @@
         <h3>Article</h3>
     </div>
 </section>
+
+
+<section class="container-tab">
+    <div class="tab" data-tabset="2">
+        <button class="tablinks tab-active" data-tabset="2">New Product</button>
+        <button class="tablinks" data-tabset="2">NEW</button>
+        <button class="tablinks" data-tabset="2">FEATURED</button>
+        <div class="line2" data-tabset="2"></div>
+    </div>
+    <div class="tabcontent-container" data-tabset="2">
+
+        <div class="tabcontent tab-active" data-tabset="2">
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/product-img.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/product-img.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-timeinstrument.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-timeinstrument.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/product-img.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/product-img.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-timeinstrument.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-timeinstrument.png" alt="">
+                    </div>
+                    <div class="card-title">
+                        <h4>Judul Produk</h4>
+                    </div>
+                    <div class="card-footer">
+                        <button>Detail</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="tabcontent" data-tabset="2">
+            <h3>Paris</h3>
+            <p>Paris is the capital of France.</p>
+        </div>
+
+        <div class="tabcontent" data-tabset="2">
+            <h3>Tokyo</h3>
+            <p>Tokyo is the capital of Japan.</p>
+        </div>
+    </div>
+</section>
+
+<section class="categories-home">
+    <div class="categories-content"></div>
+    <div class="categories-content"></div>
+    <div class="categories-content"></div>
+    <div class="categories-content"></div>
+    <div class="categories-content"></div>
+    <div class="categories-content"></div>
+    <div class="categories-content"></div>
+</section>
+
+<footer>
+    <div class="footer-container">
+        <div class="logo-content">
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_footer.png" alt="">
+            <div class="footer-contact">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/telephone-ft.svg" alt="" class="icon_footer">
+                <span>021 8690 6777</span>
+            </div>
+            <div class="footer-contact">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/wa-ft.svg" alt="" class="icon_footer">
+                <span>0819 4401 4959</span>
+            </div>
+            <div class="footer-contact">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/wa-ft.svg" alt="" class="icon_footer">
+                <span>0852 1556 0669</span>
+            </div>
+            <div class="footer-contact">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mail-ft.svg" alt="" class="icon_footer">
+                <span>@taharica.com</span>
+            </div>
+            <div class="footer-contact">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/map-ft.svg" alt="" class="icon_footer">
+                <span>Jl. Radin Inten II No. 62 Duren Sawit - Jakarta 13440</span>
+            </div>
+        </div>
+        <div class="menu-footer">
+            <h2>Menu</h2>
+            <a href="">Beranda</a>
+            <a href="">Produk</a>
+            <a href="">Menu</a>
+        </div>
+    </div>
+
+
+</footer>
 
 
 
--- js/main.js
+++ js/main.js
@@ -13,20 +13,40 @@ navLink.forEach((item) => {
 });
 
 const tabs = document.querySelectorAll(".tablinks");
-const all_content = document.querySelectorAll(".tabcontent");
+const all_contents = document.querySelectorAll(".tabcontent");
 
-tabs.forEach((tab, index) => {
+tabs.forEach((tab) => {
   tab.addEventListener("click", (e) => {
-    tabs.forEach((tab) => {
-      tab.classList.remove("tab-active");
-    });
+    const tabset = tab.getAttribute("data-tabset");
+
+    // Deactivate all tabs and contents in the current tabset
+    const currentTabs = document.querySelectorAll(
+      `.tablinks[data-tabset="${tabset}"]`
+    );
+    currentTabs.forEach((t) => t.classList.remove("tab-active"));
+
+    const currentContents = document.querySelectorAll(
+      `.tabcontent[data-tabset="${tabset}"]`
+    );
+    currentContents.forEach((content) =>
+      content.classList.remove("tab-active")
+    );
+
+    // Activate clicked tab and its corresponding content
     tab.classList.add("tab-active");
-    let line = document.querySelector(".line");
+
+    // Adjust line under the active tab
+    let line = document.querySelector(`.line[data-tabset="${tabset}"]`);
+    if (!line) {
+      line = document.querySelector(`.line2[data-tabset="${tabset}"]`);
+    }
     line.style.width = e.target.offsetWidth + "px";
     line.style.left = e.target.offsetLeft + "px";
 
-    all_content.forEach((content) => [content.classList.remove("tab-active")]);
-    all_content[index].classList.add("tab-active");
+    const contentToShow = document.querySelectorAll(
+      `.tabcontent-container[data-tabset="${tabset}"] .tabcontent`
+    )[Array.from(currentTabs).indexOf(tab)];
+    contentToShow.classList.add("tab-active");
   });
 });
 
