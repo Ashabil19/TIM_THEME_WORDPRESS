174a6f7e077df9916e2a4738a8747a1b3c68255c
Ashabil19 <ashabil@taharica.com>
2024-09-18 10:43:12 -0700

    initial commited

--- front-page.php
+++ front-page.php
@@ -38,8 +38,6 @@
         <li><a href="#">Produk Terlaris</a></li>
         <li><a href="#">Hubungi Kami</a></li>
     </ul>
-
-
     <div class="hamburger-icon">
         <span></span>
         <span></span>
