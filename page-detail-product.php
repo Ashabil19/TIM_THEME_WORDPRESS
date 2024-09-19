f663fc3315167cc751c0305bdadfee32970f6299
Fikriwib12 <wibfikri@gmail.com>
2024-09-19 19:06:49 +0700

    fixing error javascript and fixing padding margin

--- page-detail-product.php
+++ page-detail-product.php
@@ -45,15 +45,15 @@
     </section>
 
     <section id="container-tab-product">
-        <div class="tab-product" data-tabset="2">
-            <button class="tablinks-product tab-product-active" data-tabset="2">
+        <div class="tab-product" data-tabsProductet="3">
+            <button class="tablinks-product tab-product-active" data-tabsProductet="3">
                 Spesifikasi
             </button>
-            <button class="tablinks-product" data-tabset="2">Fitur</button>
-            <button class="tablinks-product" data-tabset="2">What u get</button>
+            <button class="tablinks-product" data-tabsProductet="3">Fitur</button>
+            <button class="tablinks-product" data-tabsProductet="3">What u get</button>
         </div>
-        <div class="tabcontent-product-container" data-tabset="2">
-            <div class="tabcontent-product tab-product-active" data-tabset="2">
+        <div class="tabcontent-product-container" data-tabsProductet="3">
+            <div class="tabcontent-product tab-product-active" data-tabsProductet="3">
                 <h1>Spesifikasi</h1>
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa
@@ -74,12 +74,12 @@
                 </p>
             </div>
 
-            <div class="tabcontent-product" data-tabset="2">
+            <div class="tabcontent-product" data-tabsProductet="3">
                 <h3>Paris</h3>
                 <p>Paris is the capital of France.</p>
             </div>
 
-            <div class="tabcontent-product" data-tabset="2">
+            <div class="tabcontent-product" data-tabsProductet="3">
                 <h3>Tokyo</h3>
                 <p>Tokyo is the capital of Japan.</p>
             </div>
