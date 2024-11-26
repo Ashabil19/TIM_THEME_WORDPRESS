63608f848b8c43ddec1b90524f4cf1f22c0bd8b7
ashabilsyauqi <ashabilsyauqi@gmail.com>
2024-11-26 13:40:06 +0700

    Revert "nambahin code verif google"
    This reverts commit aef6302224424e176e6702c4b283944b4dcc482b.

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="google5fece82753454f63.html">
-
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/article-page.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/detail-product-page.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/product-page.css">
